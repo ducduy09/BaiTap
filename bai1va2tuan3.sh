48fb8d70955a1c8d150453c908a5589a0e683c87
ducduy09 <nguyenducduy22092001@gmal.com>
2022-07-05 01:22:58 -0700

    BaiTap

---
+++ bai1va2tuan3.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+set `wc -w -l -c bai1.doc`
+echo "file bai1.doc co $1 dong, $2 tu, $3 ki"
+$date
+set `date`
+echo "hom nay la $1, $3 th, mouth $2, $6"
+if [$4 -eq `16:28:00`] then
+set `poweroff`fi
+exit 0
